8d84dee3b61d9fa7443c36d798027a2062737ca8
Rob Rix <rob.rix@github.com>
2015-12-31 15:23:10 -0500

    `freshBy` uses `prime`.

--- src/Data/Name.hs
+++ src/Data/Name.hs
@@ -16,9 +16,7 @@ instance Show Name where
   show (Global name) = name
 
 freshBy :: (Name -> Bool) -> Name -> Name
-freshBy used name | used name = case name of
-  Local i -> freshBy used (Local $ i + 1)
-  Global n -> freshBy used (Global $ n ++ "'")
+freshBy used name | used name = freshBy used (prime name)
 freshBy _ name = name
 
 fresh :: Set Name -> Name -> Name
