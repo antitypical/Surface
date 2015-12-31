83facf49a741f8a60a322da37c14e1764e264368
Rob Rix <rob.rix@github.com>
2015-12-30 20:55:53 -0500

    Add a smart constructor for abstractions.

--- src/Term.hs
+++ src/Term.hs
@@ -7,3 +7,6 @@ data Term f = Term { freeVariables :: [Name], expression :: Binding f (Term f) }
 
 variable :: Name -> Term f
 variable name = Term [ name ] (Variable name)
+
+abstraction :: Name -> Term f -> Term f
+abstraction name body = Term (filter (/= name) $ freeVariables body) (Abstraction name body)
