51695c61cc3a909a1987096179467e8c547c49a0
Rob Rix <rob.rix@github.com>
2015-12-31 09:59:58 -0500

    Test that `lambda` generates a fresh name.

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,7 +1,10 @@
+import Surface
 import Test.Hspec
+import Test.Hspec.QuickCheck
+import qualified Data.Set as Set
 
 main :: IO ()
 main = hspec $ do
-  describe "whatever" $ do
-    it "whatever" $
-      True `shouldBe` True
+  describe "lambda" $ do
+    it "produces binding abstractons" $
+      lambda (expression $ Type 0) id `shouldBe` Term mempty (Expression $ Lambda (Term mempty $ Expression (Type 0)) (Term mempty $ Abstraction (Local 0) $ Term (Set.singleton (Local 0)) (Variable $ Local 0)))
