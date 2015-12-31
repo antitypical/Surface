b3781e67dd7b52fee3880dd664a14c4ad50dd88d
Rob Rix <rob.rix@github.com>
2015-12-31 12:27:15 -0500

    Revert "Customize the Show instance for Expression."
    This reverts commit 959a59a65658e7e0fc1c855d0870e3c8e213db67.

--- src/Data/Expression.hs
+++ src/Data/Expression.hs
@@ -5,9 +5,4 @@ data Expression recur
   = Type Int
   | Application recur recur
   | Lambda recur recur
-  deriving (Functor, Eq, Foldable)
-
-instance Show f => Show (Expression f) where
-  show (Type n) = "Type" ++ show n
-  show (Application a b) = show a ++ " " ++ show b
-  show (Lambda t body) = "λ : " ++ show t ++ " . " ++ show body
+  deriving (Functor, Show, Eq, Foldable)
