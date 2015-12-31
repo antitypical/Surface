7e774d683df6fbd70a6fb2117df65fe900e91e2a
Rob Rix <rob.rix@github.com>
2015-12-31 12:24:13 -0500

    Revert "Use `genericReplicate` to skip a `fromIntegral`."
    This reverts commit 10021aaf1ad49f6d5f4e0eeb10ed2e2a7c53967a.

--- src/Data/Name/Internal.hs
+++ src/Data/Name/Internal.hs
@@ -8,7 +8,7 @@ module Data.Name.Internal (
 import qualified Data.List as List
 
 digits :: Integral a => a -> a -> [a]
-digits base i = fst $ foldr nextDigit ([], i) (List.genericReplicate (countDigits base i) 0)
+digits base i = fst $ foldr nextDigit ([], i) (replicate (fromIntegral $ countDigits base i) 0)
   where nextDigit _ (list, prev) | (next, remainder) <- prev `divMod` base = (remainder : list, next)
 
 countDigits :: Integral a => a -> a -> a
