2694f8bf2919777e136f96a15aa6b46d09ef9a55
Rob Rix <rob.rix@github.com>
2015-12-31 10:08:41 -0500

    Qualify the import of Set.

--- src/Data/Term.hs
+++ src/Data/Term.hs
@@ -3,15 +3,15 @@ module Data.Term where
 import Data.Binding
 import Data.Foldable
 import Data.Name
-import Data.Set
+import qualified Data.Set as Set
 
-data Term f = Term { freeVariables :: Set Name, out :: Binding f (Term f) }
+data Term f = Term { freeVariables :: Set.Set Name, out :: Binding f (Term f) }
 
 variable :: Name -> Term f
-variable name = Term (singleton name) (Variable name)
+variable name = Term (Set.singleton name) (Variable name)
 
 abstraction :: Name -> Term f -> Term f
-abstraction name body = Term (delete name $ freeVariables body) (Abstraction name body)
+abstraction name body = Term (Set.delete name $ freeVariables body) (Abstraction name body)
 
 expression :: Foldable f => f (Term f) -> Term f
 expression e = Term (foldMap freeVariables e) (Expression e)
