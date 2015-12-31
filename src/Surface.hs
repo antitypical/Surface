cc1edd873c84e7e534894ec7e1c2dee419a26ef2
Rob Rix <rob.rix@github.com>
2015-12-31 09:42:17 -0500

    Add a circular Lambda constructor.

--- src/Surface.hs
+++ src/Surface.hs
@@ -6,3 +6,9 @@ import Data.Binding as Surface
 import Data.Expression as Surface
 import Data.Name as Surface
 import Data.Term as Surface
+import qualified Data.Maybe as Maybe
+
+lambda :: Term Expression -> (Term Expression -> Term Expression) -> Term Expression
+lambda t f = expression . Lambda t $ abstraction name body
+  where body = f $ variable name
+        name = Maybe.fromMaybe (Local 0) $ maxBoundVariable body
