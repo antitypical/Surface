988d3f53e5544a2b7f3c292340294a79d008ba5e
Rob Rix <rob.rix@github.com>
2015-12-31 10:58:50 -0500

    `divMod` exists, neat.

--- src/Data/Name/Internal.hs
+++ src/Data/Name/Internal.hs
@@ -3,4 +3,4 @@ module Data.Name.Internal (
 ) where
 
 digits :: Integral a => a -> a -> [a]
-digits base i = scanr (\ each prev -> (prev `div` base) `mod` base) i [0..(logBase (fromIntegral base) (fromIntegral i))]
+digits base i = scanr (\ each prev -> fst (prev `divMod` base)) i [0..(logBase (fromIntegral base) (fromIntegral i))]
