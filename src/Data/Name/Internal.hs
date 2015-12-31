ff112e7aa7992e47588b15dcac0ee3a606a31662
Rob Rix <rob.rix@github.com>
2015-12-31 10:56:12 -0500

    Add a function computing the list of digits in a specific integer.

--- src/Data/Name/Internal.hs
+++ src/Data/Name/Internal.hs
@@ -1,2 +1,6 @@
 module Data.Name.Internal (
+  digits,
 ) where
+
+digits :: Integral a => a -> a -> [a]
+digits base i = scanr (\ each prev -> (prev `div` base) `mod` base) 0 [0..(logBase (fromIntegral base) (fromIntegral i))]
