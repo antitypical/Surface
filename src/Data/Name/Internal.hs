a7ccb7c73e62226ca3ee18aa3b3eb0c56b61689b
Rob Rix <rob.rix@github.com>
2015-12-31 11:14:07 -0500

    Take the remainder.

--- src/Data/Name/Internal.hs
+++ src/Data/Name/Internal.hs
@@ -3,4 +3,4 @@ module Data.Name.Internal (
 ) where
 
 digits :: Integral a => a -> a -> [a]
-digits base i = fst $ foldr (\ _ (list, prev) -> (fst (prev `divMod` base) : list, prev `div` base)) ([], i) [1..(logBase (fromIntegral base) (fromIntegral i))]
+digits base i = fst $ foldr (\ _ (list, prev) -> (snd (prev `divMod` base) : list, prev `div` base)) ([], i) [1..(logBase (fromIntegral base) (fromIntegral i))]
