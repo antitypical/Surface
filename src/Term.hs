cf1af6479d021bc49c523259a7214ca12add105c
Rob Rix <rob.rix@github.com>
2015-12-30 20:56:54 -0500

    Term is a Functor.

--- src/Term.hs
+++ src/Term.hs
@@ -1,9 +1,11 @@
+{-# LANGUAGE DeriveFunctor #-}
 module Term where
 
 import Binding
 import Name
 
 data Term f = Term { freeVariables :: [Name], expression :: Binding f (Term f) }
+  deriving (Functor, Show, Eq)
 
 variable :: Name -> Term f
 variable name = Term [ name ] (Variable name)
