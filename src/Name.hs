1eb18fa5325a02374ab73dbf4e3f1a95c07b3614
Rob Rix <rob.rix@github.com>
2015-12-30 21:15:06 -0500

    Freshness as tested against a set.

--- src/Name.hs
+++ src/Name.hs
@@ -1,5 +1,7 @@
 module Name where
 
+import Data.Set
+
 data Name = Local Int | Global String
   deriving (Show, Eq, Ord)
 
@@ -8,3 +10,6 @@ freshBy used name | used name = case name of
   Local i -> freshBy used (Local $ i + 1)
   Global n -> freshBy used (Global $ n ++ "'")
 freshBy _ name = name
+
+fresh :: Set Name -> Name -> Name
+fresh set = freshBy (`member` set)
