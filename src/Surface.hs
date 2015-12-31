81629e72645f7f73a5a448c2300bcdd82a7c4b66
Rob Rix <rob.rix@github.com>
2015-12-31 15:05:34 -0500

    Annotate the type at the end.

--- src/Surface.hs
+++ src/Surface.hs
@@ -30,12 +30,13 @@ apply a b = expression $ Application a b
 
 instance Show (Term Expression) where
   show = fst . para (\ b -> case b of
-    Variable n -> (show n, maxBound :: Int)
+    Variable n -> (show n, maxBound)
     Abstraction _ (_, body) -> body
     Expression (Type 0) -> ("Type", maxBound)
     Expression (Type n) -> ("Type" ++ showNumeral "₀₁₂₃₄₅₆₇₈₉" n, maxBound)
     Expression (Application (_, a) (_, b)) -> (wrap 4 (<=) a ++ " " ++ wrap 4 (<) b, 4)
-    Expression e -> (show e, -1))
+    Expression e -> (show e, -1)
+    :: (String, Int))
     where wrap i op (s, j) | i `op` j = s
           wrap i _ (s, _) = "(" ++ s ++ ")"
 
