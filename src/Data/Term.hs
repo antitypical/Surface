f5f72fa04998448154e6577c324ff8c0da4670e1
Rob Rix <rob.rix@github.com>
2015-12-31 10:03:45 -0500

    Rename variables in terms.

--- src/Data/Term.hs
+++ src/Data/Term.hs
@@ -21,3 +21,9 @@ maxBoundVariable term = case out term of
   Variable _ -> Nothing
   Abstraction name _ -> Just name
   Expression e -> maximum (maxBoundVariable <$> e)
+
+rename :: (Foldable f, Functor f) => Name -> Name -> Term f -> Term f
+rename old new (Term _ binding) = case binding of
+  (Variable name) -> if name == old then variable new else variable old
+  (Abstraction name body) -> if name == old then abstraction name body else abstraction name (rename old new body)
+  (Expression body) -> expression $ rename old new <$> body
