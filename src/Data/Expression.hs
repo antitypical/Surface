959a59a65658e7e0fc1c855d0870e3c8e213db67
Rob Rix <rob.rix@github.com>
2015-12-31 12:26:49 -0500

    Customize the Show instance for Expression.

--- src/Data/Expression.hs
+++ src/Data/Expression.hs
@@ -5,4 +5,9 @@ data Expression recur
   = Type Int
   | Application recur recur
   | Lambda recur recur
-  deriving (Functor, Show, Eq, Foldable)
+  deriving (Functor, Eq, Foldable)
+
+instance Show f => Show (Expression f) where
+  show (Type n) = "Type" ++ show n
+  show (Application a b) = show a ++ " " ++ show b
+  show (Lambda t body) = "λ : " ++ show t ++ " . " ++ show body
