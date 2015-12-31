3c46194b23aa713cd8b269f9de0aa6a1ad594b42
Rob Rix <rob.rix@github.com>
2015-12-30 21:04:15 -0500

    Add a smart constructor for expressions.

--- src/Term.hs
+++ src/Term.hs
@@ -2,6 +2,7 @@ module Term where
 
 import Binding
 import Name
+import Data.Foldable
 
 data Term f = Term { freeVariables :: [Name], out :: Binding f (Term f) }
 
@@ -10,3 +11,6 @@ variable name = Term [ name ] (Variable name)
 
 abstraction :: Name -> Term f -> Term f
 abstraction name body = Term (filter (/= name) $ freeVariables body) (Abstraction name body)
+
+expression :: (Foldable f, Functor f) => f (Term f) -> Term f
+expression e = Term (foldMap freeVariables e) (Expression e)
