4ceaff29987e5b65b75cd9a7c9b2dc131db56f26
Rob Rix <rob.rix@github.com>
2015-12-31 11:50:19 -0500

    Define `digits` in terms of `countDigits`.

--- src/Data/Name/Internal.hs
+++ src/Data/Name/Internal.hs
@@ -4,7 +4,7 @@ module Data.Name.Internal (
 ) where
 
 digits :: Integral a => a -> a -> [a]
-digits base i = fst $ foldr nextDigit ([], i) [0..logBase (fromIntegral base) (fromIntegral i)]
+digits base i = fst $ foldr nextDigit ([], i) (replicate (fromIntegral $ countDigits base i) 0)
   where nextDigit _ (list, prev) | (next, remainder) <- prev `divMod` base = (remainder : list, next)
 
 countDigits :: Integral a => a -> a -> a
