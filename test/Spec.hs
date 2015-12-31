3b9ba658616b5dc1607a1e1e7da8be4c085a348d
Rob Rix <rob.rix@github.com>
2015-12-31 10:56:35 -0500

    Test digits in base 10.

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,3 +1,4 @@
+import Data.Name.Internal
 import Surface
 import Test.Hspec
 import Test.Hspec.QuickCheck
@@ -8,6 +9,13 @@ main = hspec $ do
   describe "lambda" $ do
     it "produces binding abstractons" $
       lambda (expression $ Type 0) id `shouldBe` Term mempty (Expression $ Lambda (Term mempty $ Expression (Type 0)) (Term mempty $ Abstraction (Local 0) $ Term (Set.singleton (Local 0)) (Variable $ Local 0)))
+
+  describe "digits" $ do
+    it "zero is zero in base 10" $
+      digits 10 0 `shouldBe` [0]
+
+    it "10 has two diits in base 10" $
+      digits 10 10 `shouldBe` [1, 0]
 
 identity :: Term Expression
 identity = lambda _type' $ \ t -> lambda t id
