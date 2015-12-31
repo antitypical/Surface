a5e7cef79cb3023570e3a5ee3d0493e22572578c
Rob Rix <rob.rix@github.com>
2015-12-31 09:27:08 -0500

    Export all the symbols under Surface.

---
+++ src/Surface.hs
@@ -0,0 +1,11 @@
+module Surface (
+  module Data.Binding,
+  module Data.Expression,
+  module Data.Name,
+  module Data.Term,
+) where
+
+import Data.Name
+import Data.Binding
+import Data.Expression
+import Data.Term
