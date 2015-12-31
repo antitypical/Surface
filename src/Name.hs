09726c65d3fd95e7c994febe6ef00ed957cf31d0
Rob Rix <rob.rix@github.com>
2015-12-30 21:14:05 -0500

    Compute fresh names.

--- src/Name.hs
+++ src/Name.hs
@@ -2,3 +2,9 @@ module Name where
 
 data Name = Local Int | Global String
   deriving (Show, Eq, Ord)
+
+freshBy :: (Name -> Bool) -> Name -> Name
+freshBy used name | used name = case name of
+  Local i -> freshBy used (Local $ i + 1)
+  Global n -> freshBy used (Global $ n ++ "'")
+freshBy _ name = name
