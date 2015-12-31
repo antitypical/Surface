155dcca564e526ced495815d5586df82c02687fb
Rob Rix <rob.rix@github.com>
2015-12-31 09:41:45 -0500

    Sort the imports.

--- src/Surface.hs
+++ src/Surface.hs
@@ -2,7 +2,7 @@ module Surface (
   module Surface,
 ) where
 
-import Data.Name as Surface
 import Data.Binding as Surface
 import Data.Expression as Surface
+import Data.Name as Surface
 import Data.Term as Surface
