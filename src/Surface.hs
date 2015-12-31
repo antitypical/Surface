759389bef01c772a3681f221ae2cb60ce93e1587
Rob Rix <rob.rix@github.com>
2015-12-31 15:00:08 -0500

    Higher precedence implies no need to parenthesize.

--- src/Surface.hs
+++ src/Surface.hs
@@ -30,10 +30,10 @@ apply a b = expression $ Application a b
 
 instance Show (Term Expression) where
   show = fst . para (\ b -> case b of
-    Variable n -> (show n, -1)
+    Variable n -> (show n, maxBound :: Int)
     Abstraction _ (_, body) -> body
-    Expression (Type 0) -> ("Type", -1)
-    Expression (Type n) -> ("Type" ++ showNumeral "₀₁₂₃₄₅₆₇₈₉" n, -1)
+    Expression (Type 0) -> ("Type", maxBound)
+    Expression (Type n) -> ("Type" ++ showNumeral "₀₁₂₃₄₅₆₇₈₉" n, maxBound)
     Expression (Application (_, a) (_, b)) -> (wrap 4 (<=) a ++ " " ++ wrap 4 (<) b, 4)
     Expression e -> (show e, -1))
     where wrap i op (s, j) | i `op` j = s
