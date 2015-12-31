b88ea73b329846d8a2bf3624b2b371a4d0427760
Rob Rix <rob.rix@github.com>
2015-12-31 11:50:08 -0500

    Add a `countDigits` binding.

--- src/Data/Name/Internal.hs
+++ src/Data/Name/Internal.hs
@@ -1,7 +1,11 @@
 module Data.Name.Internal (
   digits,
+  countDigits,
 ) where
 
 digits :: Integral a => a -> a -> [a]
 digits base i = fst $ foldr nextDigit ([], i) [0..logBase (fromIntegral base) (fromIntegral i)]
   where nextDigit _ (list, prev) | (next, remainder) <- prev `divMod` base = (remainder : list, next)
+
+countDigits :: Integral a => a -> a -> a
+countDigits base i = 1 + fromIntegral (length [1..logBase (fromIntegral base) (fromIntegral i)])
