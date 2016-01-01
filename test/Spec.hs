37f464dfe1a8cdd1c116c60e3c1d284049882011
Rob Rix <rob.rix@github.com>
2016-01-01 16:40:53 -0500

    Use the _type' constructor in the tests.

--- test/Spec.hs
+++ test/Spec.hs
@@ -12,7 +12,7 @@ main = hspec $ do
 
   describe "lambda" $ do
     it "produces binding abstractions" $
-      lambda (typing $ Type 0) id `shouldBe` Term mempty (const $ Right implicit) (Binding $ Expression $ Lambda (Term mempty (const $ Right implicit) $ Type 0) (Term mempty (const $ Right implicit) $ Binding $ Abstraction (Local 0) $ Term (Set.singleton (Local 0)) (const $ Right implicit) (Binding $ Variable $ Local 0)))
+      lambda _type' id `shouldBe` Term mempty (const $ Right implicit) (Binding $ Expression $ Lambda (Term mempty (const $ Right implicit) $ Type 0) (Term mempty (const $ Right implicit) $ Binding $ Abstraction (Local 0) $ Term (Set.singleton (Local 0)) (const $ Right implicit) (Binding $ Variable $ Local 0)))
 
 identity :: Term Expression
 identity = lambda _type' $ \ t -> lambda t id
