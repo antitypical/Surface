ac6b032a50790892d037c093473adb1012901e53
Rob Rix <rob.rix@github.com>
2016-01-01 18:25:07 -0500

    Use the convenience constructors to test `lambda`.

--- test/Spec.hs
+++ test/Spec.hs
@@ -2,7 +2,6 @@ import Surface
 import qualified Data.Name.Internal.Spec
 import qualified Data.Term.Spec
 import Test.Hspec
-import qualified Data.Set as Set
 
 main :: IO ()
 main = hspec $ do
@@ -11,7 +10,7 @@ main = hspec $ do
 
   describe "lambda" $ do
     it "produces binding abstractions" $
-      lambda _type' id `shouldBe` Term mempty (const $ Right implicit) (Binding $ Expression $ Lambda (Term mempty (const $ Right implicit) $ Type 0) (Term mempty (const $ Right implicit) $ Binding $ Abstraction (Local 0) $ Term (Set.singleton (Local 0)) (const $ Right implicit) (Binding $ Variable $ Local 0)))
+      lambda _type' id `shouldBe` Term mempty (const $ Right implicit) (Binding $ Expression $ Lambda _type' (abstraction (Local 0) $ variable (Local 0)))
 
     it "rejects non-Type types" $
       typeOf (lambda _type' $ \ a -> lambda a $ \ a' -> lambda a' $ const _type') mempty `shouldBe` Left ""
