8d6c319e40224bbf54df9b5da9490cba9df28daa
Rob Rix <rob.rix@github.com>
2015-12-31 19:02:26 -0500

    Move the Data.Name.Internal tests into their own module.

---
+++ test/NameSpec.hs
@@ -0,0 +1,46 @@
+module NameSpec where
+
+import Data.Name.Internal
+import Test.Hspec
+import Test.Hspec.QuickCheck
+
+spec :: Spec
+spec = do
+  describe "digits" $ do
+    it "zero is zero in base 10" $
+      digits 10 0 `shouldBe` [0]
+
+    it "10 has two digits in base 10" $
+      digits 10 10 `shouldBe` [1, 0]
+
+    it "1234’s digits are produced in order in base 10" $
+      digits 10 1234 `shouldBe` [1, 2, 3, 4]
+
+    it "produces two digits for 255 in base 16" $
+      digits 16 255 `shouldBe` [15, 15]
+
+  describe "showNumeral" $ do
+    it "selects single letters in an alphabet" $
+      showNumeral "a" 0 `shouldBe` "a"
+
+    it "handles indices into degenerate 0-length alphabets" $
+      showNumeral "" 0 `shouldBe` ""
+
+    it "handles out-of-bounds indices into degenerate 1-length alphabets" $
+      showNumeral "a" 1 `shouldBe` "a"
+
+    it "handles out-of-bounds indices into reasonable alphabets" $
+      showNumeral "ab" 2 `shouldBe` "ba"
+
+  describe "interleave" $ do
+    prop "ignores empty lists at left" $
+      \ a -> "" `interleave` a `shouldBe` a
+
+    prop "ignores empty lists at right" $
+      \ a -> a `interleave` "" `shouldBe` a
+
+    prop "is as long as both its arguments combined" $
+      \ a b -> length (a `interleave` b) `shouldBe` length a + length (b :: String)
+
+    prop "is left-biased" $
+      \ a b -> ('a' : a) `interleave` b `shouldBe` 'a' : (b `interleave` a)
--- test/Spec.hs
+++ test/Spec.hs
@@ -1,11 +1,14 @@
 import Data.Name.Internal
 import Surface
+import qualified NameSpec
 import Test.Hspec
 import Test.Hspec.QuickCheck
 import qualified Data.Set as Set
 
 main :: IO ()
 main = hspec $ do
+  describe "Name" NameSpec.spec
+  
   describe "lambda" $ do
     it "produces binding abstractons" $
       lambda (typing $ Type 0) id `shouldBe` Term mempty Nothing (Binding $ Expression $ Lambda (Term mempty Nothing $ Type 0) (Term mempty Nothing $ Binding $ Abstraction (Local 0) $ Term (Set.singleton (Local 0)) Nothing (Binding $ Variable $ Local 0)))
@@ -40,45 +43,6 @@ main = hspec $ do
 
     it "renders lambdas without abstractions as function types" $
       show (_type' --> _type') `shouldBe` "Type → Type"
-
-  describe "digits" $ do
-    it "zero is zero in base 10" $
-      digits 10 0 `shouldBe` [0]
-
-    it "10 has two digits in base 10" $
-      digits 10 10 `shouldBe` [1, 0]
-
-    it "1234’s digits are produced in order in base 10" $
-      digits 10 1234 `shouldBe` [1, 2, 3, 4]
-
-    it "produces two digits for 255 in base 16" $
-      digits 16 255 `shouldBe` [15, 15]
-
-  describe "showNumeral" $ do
-    it "selects single letters in an alphabet" $
-      showNumeral "a" 0 `shouldBe` "a"
-
-    it "handles indices into degenerate 0-length alphabets" $
-      showNumeral "" 0 `shouldBe` ""
-
-    it "handles out-of-bounds indices into degenerate 1-length alphabets" $
-      showNumeral "a" 1 `shouldBe` "a"
-
-    it "handles out-of-bounds indices into reasonable alphabets" $
-      showNumeral "ab" 2 `shouldBe` "ba"
-
-  describe "interleave" $ do
-    prop "ignores empty lists at left" $
-      \ a -> "" `interleave` a `shouldBe` a
-
-    prop "ignores empty lists at right" $
-      \ a -> a `interleave` "" `shouldBe` a
-
-    prop "is as long as both its arguments combined" $
-      \ a b -> length (a `interleave` b) `shouldBe` length a + length (b :: String)
-
-    prop "is left-biased" $
-      \ a b -> ('a' : a) `interleave` b `shouldBe` 'a' : (b `interleave` a)
 
 identity :: Term Expression
 identity = lambda _type' $ \ t -> lambda t id
