d2ba65dfef9fd62cd2e85ed9c796b5b8bd2410aa
Rob Rix <rob.rix@github.com>
2016-01-01 17:13:25 -0500

    Ascribe the logarithm to Double.

--- src/Data/Name/Internal.hs
+++ src/Data/Name/Internal.hs
@@ -12,7 +12,7 @@ digits base i = fst $ foldr nextDigit ([], i) (replicate (fromIntegral $ countDi
   where nextDigit _ (list, prev) | (next, remainder) <- prev `divMod` base = (remainder : list, next)
 
 countDigits :: Integral a => a -> a -> a
-countDigits base i = 1 + floor (logBase (fromIntegral base) (fromIntegral $ abs i))
+countDigits base i = 1 + floor (logBase (fromIntegral base) (fromIntegral $ abs i) :: Double)
 
 showNumeral :: Integral i => String -> i -> String
 showNumeral "" _ = ""
