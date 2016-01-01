162e41ced568c9eba0c77bbc8564ae9521fc4b8c
Rob Rix <rob.rix@github.com>
2016-01-01 17:10:48 -0500

    Ascribe a bunch of numeral types.

--- test/Data/Name/Internal/Spec.hs
+++ test/Data/Name/Internal/Spec.hs
@@ -8,29 +8,29 @@ spec :: Spec
 spec = do
   describe "digits" $ do
     it "zero is zero in base 10" $
-      digits 10 0 `shouldBe` [0]
+      digits 10 0 `shouldBe` [0 :: Integer]
 
     it "10 has two digits in base 10" $
-      digits 10 10 `shouldBe` [1, 0]
+      digits 10 10 `shouldBe` [1, 0 :: Integer]
 
     it "1234’s digits are produced in order in base 10" $
-      digits 10 1234 `shouldBe` [1, 2, 3, 4]
+      digits 10 1234 `shouldBe` [1, 2, 3, 4 :: Integer]
 
     it "produces two digits for 255 in base 16" $
-      digits 16 255 `shouldBe` [15, 15]
+      digits 16 255 `shouldBe` [15, 15 :: Integer]
 
   describe "showNumeral" $ do
     it "selects single letters in an alphabet" $
-      showNumeral "a" 0 `shouldBe` "a"
+      showNumeral "a" (0 :: Integer) `shouldBe` "a"
 
     it "handles indices into degenerate 0-length alphabets" $
-      showNumeral "" 0 `shouldBe` ""
+      showNumeral "" (0 :: Integer) `shouldBe` ""
 
     it "handles out-of-bounds indices into degenerate 1-length alphabets" $
-      showNumeral "a" 1 `shouldBe` "a"
+      showNumeral "a" (1 :: Integer) `shouldBe` "a"
 
     it "handles out-of-bounds indices into reasonable alphabets" $
-      showNumeral "ab" 2 `shouldBe` "ba"
+      showNumeral "ab" (2 :: Integer) `shouldBe` "ba"
 
   describe "interleave" $ do
     prop "ignores empty lists at left" $
