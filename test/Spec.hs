ca936200fd8ee79c0a0a313f4978372483f038d0
Rob Rix <rob.rix@github.com>
2016-01-01 17:49:52 -0500

    Test that lambda’s type parameter is expected to be in Type.

--- test/Spec.hs
+++ test/Spec.hs
@@ -12,3 +12,6 @@ main = hspec $ do
   describe "lambda" $ do
     it "produces binding abstractions" $
       lambda _type' id `shouldBe` Term mempty (const $ Right implicit) (Binding $ Expression $ Lambda (Term mempty (const $ Right implicit) $ Type 0) (Term mempty (const $ Right implicit) $ Binding $ Abstraction (Local 0) $ Term (Set.singleton (Local 0)) (const $ Right implicit) (Binding $ Variable $ Local 0)))
+
+    it "rejects non-Type types" $
+      typeOf (lambda _type' $ \ a -> lambda a $ \ a' -> lambda a' $ const _type') mempty `shouldBe` Left ""
