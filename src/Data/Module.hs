7d5a4bbbf4f025a694e3e66cbe4dac0f782e4bb8
Rob Rix <rob.rix@github.com>
2016-01-01 01:42:27 -0500

    Modules have definitions.

--- src/Data/Module.hs
+++ src/Data/Module.hs
@@ -1,3 +1,7 @@
 module Data.Module where
 
-data Module = Module
+import Data.Expression
+import Data.Term
+import qualified Data.Map as Map
+
+data Module = Module (Map.Map String (Term Expression))
