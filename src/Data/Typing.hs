b57c8651ecb6e3345d716f5b9da560d43a89bd7f
Rob Rix <rob.rix@github.com>
2016-01-01 20:51:50 -0500

    Revert "Typing is Typeable."
    This reverts commit 6e3e2d73b85189d9218529202024db5032eec6e1.

--- src/Data/Typing.hs
+++ src/Data/Typing.hs
@@ -1,11 +1,9 @@
-{-# LANGUAGE DeriveFunctor, DeriveFoldable, DeriveDataTypeable #-}
+{-# LANGUAGE DeriveFunctor, DeriveFoldable #-}
 module Data.Typing where
-
-import qualified Data.Data as Data
 
 data Typing f term
   = Type Int
   | Annotation term term
   | Binding (f term)
   | Implicit
-  deriving (Show, Eq, Functor, Foldable, Data.Typeable)
+  deriving (Show, Eq, Functor, Foldable)
