d934f0c3775d32f968f70cd99e9a06a14b484cb5
Rob Rix <rob.rix@github.com>
2016-01-01 19:19:33 -0500

    Expression is Data.

--- src/Data/Expression.hs
+++ src/Data/Expression.hs
@@ -6,4 +6,4 @@ import qualified Data.Data as Data
 data Expression recur
   = Application recur recur
   | Lambda recur recur
-  deriving (Functor, Show, Eq, Foldable, Data.Typeable)
+  deriving (Functor, Show, Eq, Foldable, Data.Typeable, Data.Data)
