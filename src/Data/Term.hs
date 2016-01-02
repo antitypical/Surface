6069db3c64c4eef25f53957e61d43859036111d4
Rob Rix <rob.rix@github.com>
2016-01-02 10:19:48 -0500

    The body of an abstraction is its scope.

--- src/Data/Term.hs
+++ src/Data/Term.hs
@@ -71,7 +71,7 @@ maxBoundVariable = cata $ \ t -> case t of
 rename :: (Foldable f, Functor f, Unifiable (f (Term f)), Eq (f (Term f))) => Name -> Name -> Term f -> Term f
 rename old new term@(Term _ type' binding) = case binding of
   Binding (Variable name) -> if name == old then variable new else variable old
-  Binding (Abstraction name body) -> if name == old then abstraction name body else abstraction name (rename old new body)
+  Binding (Abstraction name scope) -> if name == old then abstraction name scope else abstraction name (rename old new scope)
   Binding (Expression body) -> checkedExpression type' $ rename old new <$> body
   Annotation a b -> let a' = rename old new a
                         b' = rename old new b in checkedTyping (check b' a') (Annotation a' b')
