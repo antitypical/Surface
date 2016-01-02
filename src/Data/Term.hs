89443ae68b2aae82abb0d5352c38fab9f2720b0b
Rob Rix <rob.rix@github.com>
2016-01-01 21:43:05 -0500

    Unification depends on equatable expressions, not terms.

--- src/Data/Term.hs
+++ src/Data/Term.hs
@@ -111,9 +111,9 @@ byUnifying a b context = do
 instance Eq (f (Term f)) => Eq (Term f) where
   a == b = freeVariables a == freeVariables b && out a == out b
 
-instance (Eq (Term f), Unifiable (f (Term f))) => Unifiable (Term f) where
+instance (Eq (f (Term f)), Unifiable (f (Term f))) => Unifiable (Term f) where
   unify expected actual = case (out expected, out actual) of
-    (_, _) | expected == actual -> Just expected
+    (a, b) | a == b -> Just expected
     (_, Implicit) -> Just expected
     (Implicit, _) -> Just actual
     (Type _, Type _) -> Just expected
