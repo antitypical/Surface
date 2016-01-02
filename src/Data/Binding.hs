b2d0655a7730cf22e365f4daebb5a4cc1b65ab88
Rob Rix <rob.rix@github.com>
2016-01-01 20:51:52 -0500

    Revert "Binding is Typeable."
    This reverts commit ffe9a7c6e03190bf43c552f372e6f588000db586.

--- src/Data/Binding.hs
+++ src/Data/Binding.hs
@@ -1,11 +1,10 @@
-{-# LANGUAGE DeriveFunctor, DeriveFoldable, DeriveDataTypeable #-}
+{-# LANGUAGE DeriveFunctor, DeriveFoldable #-}
 module Data.Binding where
 
 import Data.Name
-import qualified Data.Data as Data
 
 data Binding f term
   = Variable Name
   | Abstraction Name term
   | Expression (f term)
-  deriving (Show, Eq, Functor, Foldable, Data.Typeable)
+  deriving (Show, Eq, Functor, Foldable)
