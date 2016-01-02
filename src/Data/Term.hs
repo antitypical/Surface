e09fa69d408845d1a03ade7929c848a3ec7865aa
Rob Rix <rob.rix@github.com>
2016-01-02 10:24:05 -0500

    Rename the typechecker binding.

--- src/Data/Term.hs
+++ src/Data/Term.hs
@@ -69,10 +69,10 @@ maxBoundVariable = cata $ \ t -> case t of
   _ -> Nothing
 
 rename :: (Foldable f, Functor f, Unifiable (f (Term f)), Eq (f (Term f))) => Name -> Name -> Term f -> Term f
-rename old new term@(Term _ type' binding) = case binding of
+rename old new term@(Term _ typeChecker binding) = case binding of
   Binding (Variable name) -> if name == old then variable new else variable old
   Binding (Abstraction name scope) -> if name == old then term else abstraction name (rename old new scope)
-  Binding (Expression body) -> checkedExpression type' $ rename old new <$> body
+  Binding (Expression body) -> checkedExpression typeChecker $ rename old new <$> body
   Annotation a b -> let a' = rename old new a
                         b' = rename old new b in checkedTyping (check b' a') (Annotation a' b')
   Type _ -> term
