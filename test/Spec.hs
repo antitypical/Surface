011a9cbb1b4998ccd56326ff705b1a09f8faf3f6
Rob Rix <rob.rix@github.com>
2016-01-01 22:15:33 -0500

    Test that the rejection of non-Type types satisfies isLeft, rather than testing for a specific message.

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,6 +1,7 @@
 import Surface
 import qualified Data.Name.Internal.Spec
 import qualified Data.Term.Spec
+import qualified Data.Either as Either
 import Test.Hspec
 
 main :: IO ()
@@ -13,11 +14,11 @@ main = hspec $ do
       lambda _type' id `shouldBe` Term mempty (const $ Right implicit) (Binding $ Expression $ Lambda _type' (abstraction (Local 0) $ variable (Local 0)))
 
     it "rejects non-Type types" $
-      typeOf (lambda _type' $ \ a -> lambda a $ \ a' -> lambda a' $ const _type') mempty `shouldBe` Left ""
+      typeOf (lambda _type' $ \ a -> lambda a $ \ a' -> lambda a' $ const _type') mempty `shouldSatisfy` Either.isLeft
 
   describe "-->" $ do
     it "rejects non-Type parameter types" $
-      typeOf (lambda _type' $ \ a -> lambda a $ \ a' -> a' --> a) mempty `shouldBe` Left ""
+      typeOf (lambda _type' $ \ a -> lambda a $ \ a' -> a' --> a) mempty `shouldSatisfy` Either.isLeft
 
     it "rejects non-Type return types" $
-      typeOf (lambda _type' $ \ a -> lambda a $ \ a' -> a --> a') mempty `shouldBe` Left ""
+      typeOf (lambda _type' $ \ a -> lambda a $ \ a' -> a --> a') mempty `shouldSatisfy` Either.isLeft
