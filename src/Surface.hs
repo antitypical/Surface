05af14a7638352418e732dfdcc6e226bf01d250f
Rob Rix <rob.rix@github.com>
2016-01-01 22:08:28 -0500

    `pi` associates to the right.

--- src/Surface.hs
+++ src/Surface.hs
@@ -31,6 +31,8 @@ lambda t f = checkedExpression type' $ Lambda t body
           body' <- typeOf body (Map.insert name t context)
           return $ t --> body'
 
+infixr `pi`
+
 -- | Construct a pi type from a type and a function from an argument variable to the resulting type. The variable will be picked automatically. The parameter type will be checked against `Type`, as will the substitution of the parameter type into the body.
 pi :: Term Expression -> (Term Expression -> Term Expression) -> Term Expression
 pi t f = checkedExpression type' $ Lambda t body
