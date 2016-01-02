6e3e2d73b85189d9218529202024db5032eec6e1
Rob Rix <rob.rix@github.com>
2016-01-01 19:15:12 -0500

    Typing is Typeable.

--- src/Data/Typing.hs
+++ src/Data/Typing.hs
@@ -1,9 +1,11 @@
-{-# LANGUAGE DeriveFunctor, DeriveFoldable #-}
+{-# LANGUAGE DeriveFunctor, DeriveFoldable, DeriveDataTypeable #-}
 module Data.Typing where
+
+import qualified Data.Data as Data
 
 data Typing f term
   = Type Int
   | Annotation term term
   | Binding (f term)
   | Implicit
-  deriving (Show, Eq, Functor, Foldable)
+  deriving (Show, Eq, Functor, Foldable, Data.Typeable)
