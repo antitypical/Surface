e706dcc4f09cd63e43e1c2b82d624542fe1b342b
Rob Rix <rob.rix@github.com>
2016-01-01 20:57:33 -0500

    Expression is Unifiable.

--- src/Data/Expression.hs
+++ src/Data/Expression.hs
@@ -1,7 +1,21 @@
 {-# LANGUAGE DeriveFunctor, DeriveFoldable #-}
 module Data.Expression where
 
+import Data.Unification
+
 data Expression recur
   = Application recur recur
   | Lambda recur recur
   deriving (Functor, Show, Eq, Foldable)
+
+instance Unifiable recur => Unifiable (Expression recur) where
+  unify expected actual = case (expected, actual) of
+    (Application a1 b1, Application a2 b2) -> do
+      a <- unify a1 a2
+      b <- unify b1 b2
+      return $ Application a b
+    (Lambda a1 b1, Lambda a2 b2) -> do
+      a <- unify a1 a2
+      b <- unify b1 b2
+      return $ Lambda a b
+    _ -> Nothing
