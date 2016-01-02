ffe9a7c6e03190bf43c552f372e6f588000db586
Rob Rix <rob.rix@github.com>
2016-01-01 19:15:07 -0500

    Binding is Typeable.

--- src/Data/Binding.hs
+++ src/Data/Binding.hs
@@ -1,10 +1,11 @@
-{-# LANGUAGE DeriveFunctor, DeriveFoldable #-}
+{-# LANGUAGE DeriveFunctor, DeriveFoldable, DeriveDataTypeable #-}
 module Data.Binding where
 
 import Data.Name
+import qualified Data.Data as Data
 
 data Binding f term
   = Variable Name
   | Abstraction Name term
   | Expression (f term)
-  deriving (Show, Eq, Functor, Foldable)
+  deriving (Show, Eq, Functor, Foldable, Data.Typeable)
