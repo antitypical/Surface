3239847583e1ef2a2ee90a6f39a0b184ed6434e9
Rob Rix <rob.rix@github.com>
2016-01-02 15:16:36 -0500

    `lambda` and `pi` associate to the right.

--- src/Surface.hs
+++ src/Surface.hs
@@ -18,6 +18,8 @@ import Data.Typing as Surface'
 import Data.Unification as Surface'
 import qualified Data.Set as Set
 
+infixr `lambda`
+
 -- | Construct a lambda from a type and a function from an argument variable to the resulting term. The variable will be picked automatically. The parameter type will be checked against `Type`, but there are no constraints on the type of the result.
 lambda :: Term Expression -> (Term Expression -> Term Expression) -> Term Expression
 lambda t f = checkedExpression typeChecker $ Lambda t body
@@ -28,6 +30,7 @@ lambda t f = checkedExpression typeChecker $ Lambda t body
           _ <- checkIsType t context
           body' <- typeOf body (extendContext t context body)
           return $ t `pi` (`applySubstitution` body')
+
 
 -- | Construct a pi type from a type and a function from an argument variable to the resulting type. The variable will be picked automatically. The parameter type will be checked against `Type`, as will the substitution of the parameter type into the body.
 pi :: Term Expression -> (Term Expression -> Term Expression) -> Term Expression
