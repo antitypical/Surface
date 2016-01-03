17873210dcbc9b119b6b0a9e6fdb8df076b3c22a
Rob Rix <rob.rix@github.com>
2016-01-03 13:51:06 -0500

    Lower the precedence of `shouldResult`.

--- test/Test/Assertions.hs
+++ test/Test/Assertions.hs
@@ -4,5 +4,7 @@ import Surface
 import Test.Hspec
 import Test.HUnit.Base
 
+infixl 1 `shouldResult`
+
 shouldResult :: (Show a, Eq a) => Result a -> a -> Expectation
 action `shouldResult` expected = either assertFailure (`shouldBe` expected) action
