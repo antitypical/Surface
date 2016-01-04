6a58e47d84b26920822000fe01555f060c35e4a2
Rob Rix <rob.rix@github.com>
2016-01-03 19:24:40 -0500

    Rename the Replace branch of Unification to Conflict.

--- src/Data/Term/Types.hs
+++ src/Data/Term/Types.hs
@@ -14,18 +14,18 @@ type TypeChecker term = Context term -> Result term
 
 data Term f = Term { freeVariables :: Set.Set Name, typeOf :: TypeChecker (Term f), out :: Typing (Binding f) (Term f) }
 
-data Unification f = Unification (Typing (Binding f) (Unification f)) | Replace (Term f) (Term f)
+data Unification f = Unification (Typing (Binding f) (Unification f)) | Conflict (Term f) (Term f)
 
 expected :: Functor f => Unification f -> Term f
-expected (Replace expected _) = expected
+expected (Conflict expected _) = expected
 expected (Unification out) = Term Set.empty (const $ Left "Unification does not preserve typecheckers") (expected <$> out)
 
 actual :: Functor f => Unification f -> Term f
-actual (Replace _ actual) = actual
+actual (Conflict _ actual) = actual
 actual (Unification out) = Term Set.empty (const $ Left "Unification does not preserve typecheckers") (actual <$> out)
 
 unified :: Traversable f => Unification f -> Maybe (Term f)
-unified (Replace _ _) = Nothing
+unified (Conflict _ _) = Nothing
 unified (Unification out) = do
   out <- mapM unified out
   return $ Term Set.empty (const $ Left "Unification does not preserve typecheckers") out
