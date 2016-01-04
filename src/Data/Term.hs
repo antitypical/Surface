7de15f54d3baa2ccb9bee4631e7101ee45c9bb31
Rob Rix <rob.rix@github.com>
2016-01-03 22:11:54 -0500

    Unify identically-named abstractions without renaming.

--- src/Data/Term.hs
+++ src/Data/Term.hs
@@ -134,6 +134,7 @@ unify expected actual = case (out expected, out actual) of
   (Type _, Type _) -> into expected
   (Annotation term1 type1, Annotation term2 type2) -> Unification (Annotation (unify term1 term2) (unify type1 type2))
 
+  (Binding (Abstraction name1 scope1), Binding (Abstraction name2 scope2)) | name1 == name2 -> Unification (Binding $ Abstraction name1 (unify scope1 scope2))
   (Binding (Abstraction name1 scope1), Binding (Abstraction name2 scope2)) -> Unification (Binding $ Abstraction name1 (rename' name name1 (unify (rename name1 name scope1) (rename name2 name scope2))))
     where name = if name1 == name2
             then name1
