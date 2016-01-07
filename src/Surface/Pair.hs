d94175ef3f814737152bd20c3cec02948231518f
Rob Rix <rob.rix@github.com>
2016-01-06 20:45:38 -0500

    Define a Pair type.

--- src/Surface/Pair.hs
+++ src/Surface/Pair.hs
@@ -1,1 +1,9 @@
 module Surface.Pair where
+
+import Data.Expression
+import Data.Term
+import Data.Term.Types
+import Surface.Language
+
+_Pair :: Term Expression
+_Pair = _type' `lambda` \ a -> _type' `lambda` \ b -> _type' `lambda` \ motive -> (a --> b --> motive) --> motive
