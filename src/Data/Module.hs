68a1a7138107c1fde95a0db6137f6c9c4c730736
Rob Rix <rob.rix@github.com>
2016-01-07 21:15:38 -0500

    Add a lookup operator for Module.

--- src/Data/Module.hs
+++ src/Data/Module.hs
@@ -1,8 +1,14 @@
 module Data.Module where
 
 import Data.Expression
+import qualified Data.List as List
 import Data.Term.Types
 
 data Module = Module [Definition]
+
+(!) :: Module -> String -> Definition
+Module definitions ! key = case List.find ((== key) . symbol) definitions of
+  Just x -> x
+  _ -> error "Expected definition to exist"
 
 data Definition = Definition { symbol :: String, getType :: Term Expression }
