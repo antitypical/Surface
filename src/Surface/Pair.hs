70b76007887ed406e2db7d47b7a35b05ac699df9
Rob Rix <rob.rix@github.com>
2016-01-07 21:00:23 -0500

    Stub in a Pair module.

--- src/Surface/Pair.hs
+++ src/Surface/Pair.hs
@@ -1,8 +1,15 @@
 module Surface.Pair where
 
 import Data.Expression
+import Data.Module
 import Data.Term.Types
 import Surface.Language
+import qualified Data.Map as Map
+
+_module :: Module
+_module = Module $ Map.fromList [
+    ("Pair", _Pair)
+  ]
 
 _Pair :: Term Expression
 _Pair = _type' `lambda` \ a -> _type' `lambda` \ b -> _type' `lambda` \ motive -> (a --> b --> motive) --> motive
