bdd3daa4dc593876e6c76cf1638f08619e48ff7a
Rob Rix <rob.rix@github.com>
2016-01-09 11:54:59 -0500

    Move the Unifiable instance for Expression into Surface.Unification.

--- src/Data/Expression.hs
+++ src/Data/Expression.hs
@@ -1,15 +1,7 @@
 {-# LANGUAGE DeriveFunctor, DeriveFoldable, DeriveTraversable #-}
 module Data.Expression where
 
-import Surface.Unification
-
 data Expression recur
   = Application recur recur
   | Lambda recur recur
   deriving (Functor, Show, Eq, Foldable, Traversable)
-
-instance Unifiable Expression where
-  unifyBy f expected actual = case (expected, actual) of
-    (Application a1 b1, Application a2 b2) -> Just $ Application (f a1 a2) (f b1 b2)
-    (Lambda a1 b1, Lambda a2 b2) -> Just $ Lambda (f a1 a2) (f b1 b2)
-    _ -> Nothing
--- src/Surface/Unification.hs
+++ src/Surface/Unification.hs
@@ -1,6 +1,13 @@
 module Surface.Unification where
 
+import Data.Expression
 import Data.Term.Types
 
 class Unifiable e where
   unifyBy :: (f e -> f e -> Unification e) -> e (f e) -> e (f e) -> Maybe (e (Unification e))
+
+instance Unifiable Expression where
+  unifyBy f expected actual = case (expected, actual) of
+    (Application a1 b1, Application a2 b2) -> Just $ Application (f a1 a2) (f b1 b2)
+    (Lambda a1 b1, Lambda a2 b2) -> Just $ Lambda (f a1 a2) (f b1 b2)
+    _ -> Nothing
