7a4651e63a8f638082354bf4155a346399e4b9aa
Rob Rix <rob.rix@github.com>
2016-01-09 01:52:51 -0500

    Module is defined in terms of Term'.

--- src/Data/Module.hs
+++ src/Data/Module.hs
@@ -1,6 +1,5 @@
 module Data.Module where
 
-import Data.Expression
 import qualified Data.List as List
 import Data.Term.Types
 import Surface.Language
@@ -12,10 +11,10 @@ Module definitions ! key = case List.find ((== key) . symbol) definitions of
   Just x -> x
   _ -> error "Expected definition to exist"
 
-checkModule :: Context (Term Expression) -> Module -> Result [Term Expression]
+checkModule :: Context Term' -> Module -> Result [Term']
 checkModule context (Module definitions) = sequence $ checkDefinition context <$> definitions
 
-data Definition = Definition { symbol :: String, getType :: Term Expression, getValue :: Term Expression }
+data Definition = Definition { symbol :: String, getType :: Term', getValue :: Term' }
 
-checkDefinition :: Context (Term Expression) -> Definition -> Result (Term Expression)
+checkDefinition :: Context Term' -> Definition -> Result Term'
 checkDefinition context (Definition symbol t v) = typeOf t _type' context >> typeOf v t context
